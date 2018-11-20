2c99a6e89a8a7d0096d53e2441891da83b8dadf5
Amelia <ameliaswain1@gmail.com>
2018-11-20 15:33:53 +0000

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Amelia Swain (s5115789) on 20/11/2018.
+//  Copyright © 2018 Amelia Swain (s5115789). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
